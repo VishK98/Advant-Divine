eed9cc85c968755d88280aa23386f4314fffba7d
VishK98 <visheshkumar41@gmail.com>
2024-07-04 14:24:54 +0530

    Add2Cart & Size Added

---
+++ src/Components/Men/AddCart.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { FaShoppingCart } from 'react-icons/fa';
+import Sidebar from './SideBar';
+import './Men.css';
+
+const AddCartButton = () => {
+    const [isSidebarOpen, setSidebarOpen] = useState(false);
+    const [selectedColor, setSelectedColor] = useState('');
+
+    const toggleSidebar = () => {
+        setSidebarOpen(!isSidebarOpen);
+    };
+
+    return (
+        <div>
+            {/* <div className="filter-btn" onClick={toggleSidebar}>
+                <FaShoppingCart color='black' />
+                <span style={{color: "#000", marginLeft: "15px"}}>ADD TO CART</span>
+                {selectedColor && <span className="selected-color">{selectedColor}</span>}
+            </div> */}
+            <button className='add2cart-btn'>
+                <FaShoppingCart className="icon" />&nbsp;&nbsp;&nbsp;ADD TO CART
+            </button>
+            {/* <div>
+                <button className='filter-btn'> ADD TO CART
+                </button>
+            </div> */}
+            <Sidebar
+                isOpen={isSidebarOpen}
+                toggleSidebar={toggleSidebar}
+                setSelectedColor={setSelectedColor}
+            />
+        </div>
+    );
+};
+
+export default AddCartButton;
--- src/Components/Men/Men.js
+++ src/Components/Men/Men.js
@@ -4,6 +4,7 @@ import Banner1 from './../../Assets/Images/Banner/Banner 1.png';
 import Footer from "../Footer/footer";
 import HeaderText from './HeaderText';
 import Filter from './Filter';
+import AddCart from './AddCart';
 import C1P4 from './../../Assets/Images/Product Image/Product Section1-4.png';
 import C2P1 from "../../Assets/Images/Product Image/Product Section2-1.png";
 import C2P2 from "../../Assets/Images/Product Image/Product Section2-2.png";
@@ -16,29 +17,27 @@ import CategorySlider from './CategorySlider';
 function Men() {
     const [sort, setSort] = useState('');
     const cardData = [
-        { imgSrc: C1P4, title: "View more", link: "#" },
-        { imgSrc: C1P4, title: "View more", link: "#" },
-        { imgSrc: C1P4, title: "View more", link: "#" },
-        { imgSrc: C1P4, title: "View more", link: "#" },
-        { imgSrc: C1P4, title: "View more", link: "#" },
-        { imgSrc: C1P4, title: "View more", link: "#" },
-        { imgSrc: C1P4, title: "View more", link: "#" },
-        { imgSrc: C1P4, title: "View more", link: "#" },
+        { imgSrc: C1P4, title: "TrueBrowns  Grey Cotton Coat", link: "#", price: "3799" },
+        { imgSrc: C1P4, title: "TrueBrowns  Grey Cotton Coat", link: "#", price: "3799" },
+        { imgSrc: C1P4, title: "TrueBrowns  Grey Cotton Coat", link: "#", price: "3799" },
+        { imgSrc: C1P4, title: "TrueBrowns  Grey Cotton Coat", link: "#", price: "3799" },
+        { imgSrc: C1P4, title: "TrueBrowns  Grey Cotton Coat", link: "#", price: "3799" },
+        { imgSrc: C1P4, title: "TrueBrowns  Grey Cotton Coat", link: "#", price: "3799" },
+        { imgSrc: C1P4, title: "TrueBrowns  Grey Cotton Coat", link: "#", price: "3799" },
+        { imgSrc: C1P4, title: "TrueBrowns  Grey Cotton Coat", link: "#", price: "3799" },
         // ... (rest of the cardData)
     ];
     const recentView = [
-        { imgSrc: C2P1, title: "Women", amount: "Rs. 3000", link: "#" },
-        { imgSrc: C2P2, title: "Men", amount: "Rs. 3000", link: "#" },
-        { imgSrc: C2P3, title: "Unisex", amount: "Rs. 3000", link: "#" },
+        { imgSrc: C2P1, title: "Women", link: "#", price: "3799" },
+        { imgSrc: C2P2, title: "Men", link: "#", price: "3799" },
+        { imgSrc: C2P3, title: "Unisex", link: "#", price: "3799" },
         { imgSrc: C2P4, title: "View more", link: "#" },
     ];
     const categories = [
-        { imgSrc: C1P4, title: "Category 1", link: "#" },
-        { imgSrc: C1P4, title: "Category 2", link: "#" },
-        { imgSrc: C1P4, title: "Category 3", link: "#" },
-        { imgSrc: C1P4, title: "Category 4", link: "#" },
-        { imgSrc: C1P4, title: "Category 5", link: "#" },
-        // Add more categories as needed
+        { imgSrc: C1P4, title: "Category 1", link: "#", },
+        { imgSrc: C1P4, title: "Category 2", link: "#", },
+        { imgSrc: C1P4, title: "Category 3", link: "#",  },
+        { imgSrc: C1P4, title: "Category 4", link: "#",  },
     ];
 
     return (
@@ -47,69 +46,100 @@ function Men() {
             <div className="home-container">
                 <img className="temp-image1" style={{ height: "91%" }} src={Banner1} alt="Avant Divine" />
             </div>
-            <div className=' p-1'>
+            <div className='mt-3 mt-lg-5'>
                 <HeaderText text="Mens" />
-                <div className="container-fluid">
-                    <div className="row align-items-center">
-                        <div className="col-6 col-md-1 col-lg-1">
+            </div>
+            <div className='container-fluid men-section-padding mb-lg-3'>
+                <div class="filter-sort-btn mb-lg-3">
+                    <div class="row">
+                        <div class="col-6 col-md-auto mb-2">
                             <Filter />
                         </div>
-                        <div className='col-6 col-md-2 col-lg-2'>
-                            <div className=" d-flex align-items-center">
-                                <p className="mb-2" style={{ marginRight: "10px", marginLeft: "30px" }}>Sort By :</p>
-                                <select className="mb-2 p-2" value={sort} onChange={(e) => setSort(e.target.value)}>
-                                    <option value="">Sort By</option>
-                                    <option value="sort1">High to Low</option>
-                                    <option value="sort2">Low to High</option>
+                        <div class="col-6 col-md-auto">
+                            <select class="filter-btn sort-dropdown" value={sort} onChange={(e) => setSort(e.target.value)}>
+                                <option value="default">Sort By</option>
+                                <option value="name">Name</option>
+                                <option value="date">Date</option>
+                                <option value="price">Price</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    {cardData.map((card, index) => (
+                        <div className="col-md-3 col-6 mb-lg-4 mb-3" key={index} style={{ cursor: "pointer" }}>
+                            <img
+                                className="trendsetters-image"
+                                src={card.imgSrc}
+                                alt={card.title}
+                            />
+                            <p className="card-title">{card.title}</p>
+                            <p className="card-title">₹{card.price}</p>
+                            <select className="filter-btn sort-dropdown" style={{ paddingLeft: "10px", marginTop: "10px" }} value={sort} onChange={(e) => setSort(e.target.value)}>
+                                <option value="default">Select Size</option>
+                                <option value="XS">XS</option>
+                                <option value="S">S</option>
+                                <option value="M">M</option>
+                                <option value="L">L</option>
+                                <option value="XL">XL</option>
+                            </select>
+                            <AddCart />
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <HeaderText text="Recently Viewed" />
+            <div className='container-fluid men-section-padding mb-lg-5'>
+                <div className="row">
+                    {recentView.map((card, index) => (
+                        <div className="col-md-3 col-6" key={index}>
+                            <div style={{ cursor: "pointer" }}>
+                                <img
+                                    className="trendsetters-image"
+                                    src={card.imgSrc}
+                                    alt={card.title}
+                                />
+                                <p className="card-title">{card.title}</p>
+                                <p className="card-title">₹{card.price}</p>
+                                <select className="filter-btn sort-dropdown" style={{ paddingLeft: "10px", marginTop: "10px" }} value={sort} onChange={(e) => setSort(e.target.value)}>
+                                    <option value="default">Select Size</option>
+                                    <option value="XS">XS</option>
+                                    <option value="S">S</option>
+                                    <option value="M">M</option>
+                                    <option value="L">L</option>
+                                    <option value="XL">XL</option>
                                 </select>
+                                <AddCart />
                             </div>
                         </div>
-                    </div>
-                    <div className="mt-lg-5 mt-3 ">
-                        <div className="row">
-                            {cardData.map((card, index) => (
-                                <div className="col-md-3 col-6 mb-5" key={index}>
-                                    <a href={card.link} className="card-link">
-                                        <div style={{ cursor: "pointer" }}>
-                                            <img
-                                                className="trendsetters-image"
-                                                src={card.imgSrc}
-                                                alt={card.title}
-                                            />
-                                            <h5 className="card-title">{card.title}</h5>
-                                        </div>
-                                    </a>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
+                    ))}
                 </div>
-                <div style={{ marginTop: "-40px" }}>
-                    <HeaderText text="Recently Viewed" />
-                </div>
-                <div className=" container-fluid mt-lg-5 mt-3 ">
-                    <div className="row">
-                        {recentView.map((card, index) => (
-                            <div className="col-md-3 col-6 mb-5" key={index}>
-                                <a href={card.link} className="card-link">
-                                    <div style={{ cursor: "pointer" }}>
-                                        <img
-                                            className="trendsetters-image"
-                                            src={card.imgSrc}
-                                            alt={card.title}
-                                        />
-                                        <h5 className="card-title">{card.title}</h5>
-                                    </div>
-                                </a>
+            </div>
+            <HeaderText text="Category" />
+            <div className='container-fluid men-section-padding mb-lg-5'>
+                <div className="row">
+                    {categories.map((card, index) => (
+                        <div className="col-md-3 col-6" key={index}>
+                            <div style={{ cursor: "pointer" }}>
+                                <img
+                                    className="trendsetters-image"
+                                    src={card.imgSrc}
+                                    alt={card.title}
+                                />
+                                <p className="card-title">{card.title}</p>
+                                <p className="card-title">₹{card.price}</p>
+                                <select className="filter-btn sort-dropdown" style={{ paddingLeft: "10px", marginTop: "10px" }} value={sort} onChange={(e) => setSort(e.target.value)}>
+                                    <option value="default">Select Size</option>
+                                    <option value="XS">XS</option>
+                                    <option value="S">S</option>
+                                    <option value="M">M</option>
+                                    <option value="L">L</option>
+                                    <option value="XL">XL</option>
+                                </select>
+                                <AddCart />
                             </div>
-                        ))}
-                    </div>
-                </div>
-                <div style={{ marginTop: "-40px", marginBottom: "60px" }}>
-                    <HeaderText text="Categories" />
-                </div>
-                <div className='mb-5'>
-                    <CategorySlider cards={categories} />
+                        </div>
+                    ))}
                 </div>
             </div>
             <Footer />
