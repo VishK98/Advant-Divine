4c62be60d7329c895172ab5e8af24cf8e8fd7a7c
Vishal Upadhyay <mevishalupadhyay@gmail.com>
2024-07-12 04:12:01 +0530

    done

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import logo from "../../Assets/Images/logo.jpeg";
-import { BiCart } from 'react-icons/bi';
+import { BiHeart, BiCart, BiUser, BiMenu, BiX } from 'react-icons/bi'; // Import BiMenu and BiX
 import "./Nav.css";
 
 const Navbar = () => {
@@ -36,7 +36,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className="nav__bar p-lg-2">
+    <div className={`nav__bar p-lg-2 ${showSearchBar ? 'scroll-active' : ''}`}>
       <div className="row justify-content-between align-items-center">
         <div className="col-md-2 col-3">
           <div className="first-box d-flex align-items-center">
@@ -46,7 +46,7 @@ const Navbar = () => {
               onClick={toggleMenu}
               aria-label={isMenuOpen ? "Close Menu" : "Open Menu"}
             >
-              <i className={`bi ${isMenuOpen ? "bi-x" : "bi-list"}`}></i>
+              {isMenuOpen ? <BiX /> : <BiMenu />} {/* Use BiMenu and BiX */}
             </Button>
             <span
               className="navbar__text d-none d-md-inline ms-3"
@@ -55,22 +55,12 @@ const Navbar = () => {
             >
               {isMenuOpen ? "Close" : "Menu"}
             </span>
-            {/* <Link to="/search" className="call-us-link me-3">
-              <i className="bi bi-search"></i>
-            </Link> */}
-
-            <div>
-              {/* <Link to="/search" className="mt-3 navbar__text">
-                {" "}
-                Search
-              </Link> */}
-            </div>
           </div>
         </div>
         <div className="col-md-8 col-6 text-center">
-        <Link to="/">
-  <img className="logo__image" src={logo} alt="Logo" />
-</Link>
+          <Link to="/">
+            <img className="logo__image" src={logo} alt="Company Logo" />
+          </Link>
         </div>
         <div className="col-md-2 col-3">
           <div className="third-box d-flex justify-content-end align-items-center navbar__margin">
@@ -81,27 +71,17 @@ const Navbar = () => {
               <p className="navbar__text mt-3">Call Us</p>
             </a>
             <Link to="/favorites" className="call-us-link me-3">
-              <i className="bi bi-heart"></i>
+              <BiHeart fontSize="20px" />
             </Link>
-            <Link to="/cart" className="call-us-link">
-      <span role="img" aria-label="Cart" className="cart-icon">🛒</span>
-    </Link>
+            <Link to="/cart" className="call-us-link me-3">
+              <BiCart fontSize="20px" />
+            </Link>
+            <Link to="/profile" className="call-us-link me-3">
+              <BiUser fontSize="20px" />
+            </Link>
           </div>
         </div>
       </div>
-      {/* <div className={`navbar__menu ${isMenuOpen ? "open" : ""}`}>
-        <Link to="/gifts" className="navbar__menuitem">Gifts</Link>
-        <Link to="/new" className="navbar__menuitem">New</Link>
-        <Link to="/bags" className="navbar__menuitem">Bags</Link>
-        <Link to="/women" className="navbar__menuitem">Women</Link>
-        <Link to="/men" className="navbar__menuitem">Men</Link>
-        <Link to="/jewellery" className="navbar__menuitem">Jewellery</Link>
-        <Link to="/watches" className="navbar__menuitem">Watches</Link>
-        <Link to="/perfumes" className="navbar__menuitem">Perfumes</Link>
-        <Link to="/art-of-living" className="navbar__menuitem">Art of Living</Link>
-        <Link to="/services" className="navbar__menuitem">Services</Link>
-        <Link to="/world-of-louis-vuitton" className="navbar__menuitem">World of Louis Vuitton</Link>
-      </div> */}
     </div>
   );
 };
